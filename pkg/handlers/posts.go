8270d1091698725c0b0c77792910743481e588bc
Siyovush <paraparadox@protonmail.com>
2023-04-26 08:35:17 +0500

    Implement GetOnePost method

--- pkg/handlers/posts.go
+++ pkg/handlers/posts.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/softclub-go-0-0/post-service/pkg/models"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 )
@@ -20,6 +22,22 @@ func (h *handler) GetAllPosts(c *gin.Context) {
 }
 
 func (h *handler) GetOnePost(c *gin.Context) {
+	var post models.Post
+	if err := h.DB.Preload("ProcessingResult").First(&post, c.Param("postID")).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("post nor found:", err)
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"message": "There is no such post",
+			})
+			return
+		}
+		log.Println("getting a post from DB:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, post)
 }
 
 func (h *handler) SearchPosts(c *gin.Context) {
